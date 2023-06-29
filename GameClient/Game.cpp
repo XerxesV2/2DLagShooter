48a5268e8fd654180892dc61dfe97f6d3f99bf83
XerxesV2 <89029804+XerxesV2@users.noreply.github.com>
2023-06-29 20:10:40 +0200

    Add files via upload

--- GameClient/Game.cpp
+++ GameClient/Game.cpp
@@ -1,5 +1,5 @@
 #include "Game.hpp"
-#include "LogSystem.hpp"
+#include "cl_Chat.hpp"
 #include <Windows.h>
 #include <tlhelp32.h>
 
@@ -32,11 +32,12 @@ Game::Game() : window(sf::VideoMode(1920, 1080), "gamegame")
     if (AlreadyRunning("GameClient.exe")) window.setPosition(sf::Vector2i(1920, 0));
     else window.setPosition(sf::Vector2i(0, 0));
 
-    view.setSize(1920.f, 1080.f);
-    view.setCenter(1920.f / 2.f, 1080.f / 2.f);
+    view.setSize(g_CamSizeX, g_CamSizeY);
+    view.setCenter(g_CamSizeX / 2.f, g_CamSizeY / 2.f);
     window.setView(view);
 
-    LogSystem::Get().Init(&this->window);
+    LogSystem::Get().Init(&this->window, &m_fDeltaTime);
+    LeaderBoard::Get().Init(&m_Font);
 
     window.setFramerateLimit(60);
     m_Font.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");
@@ -53,6 +54,9 @@ Game::Game() : window(sf::VideoMode(1920, 1080), "gamegame")
 
     m_pPlayers = std::make_shared<Players>(window, m_Font, m_fDeltaTime);
     m_pGameNetwork = std::make_shared<GameNetwork>(m_pPlayers, m_fDeltaTime);
+    m_pUI = std::make_shared<UI>(&window);
+
+    m_pLogin = std::make_unique<Login>(window, m_pGameNetwork);
 
     //m_mapPlayers[0].playerInfo.n_uID = 0;
     //m_mapPlayers[0].playerInfo.v_fPos = { 10.f, 10.f };
@@ -69,6 +73,19 @@ bool Game::Connect()
 
 void Game::MainLoop()
 {
+    Connect();
+
+    if (m_pLogin->MainLoop()) return;
+    m_pGameNetwork->RegisterClient();
+
+    while(!m_pGameNetwork->LocalPlayerAdded() && !g_bOfflinePLay)
+    {
+        m_pGameNetwork->ProcessIncomingPackets();
+    }
+
+    m_pUI->Init(m_pPlayers->GetLocalPLayerStat());
+    Chat::Get().Init(&window, m_pPlayers->GetLocalPLayer(), m_pGameNetwork, &m_fDeltaTime, &m_PressedKeyCodes);
+
     while (window.isOpen())
     {
         HandleEvents();
@@ -82,16 +99,15 @@ void Game::UpdateClient()
     if (m_bGameOver) return;
 
     if(!g_bOfflinePLay) m_pGameNetwork->ProcessIncomingPackets();
-    if (!m_pGameNetwork->Connected() && !g_bOfflinePLay) return;
 
     m_fDeltaTime = m_DeltaClock.restart().asSeconds();  //needs to be cloese to each other bc the server interpolation
     m_pPlayers->m_DeltaTimeTimePoint = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     //std::cout << "deltaTime: " << m_fDeltaTime << '\n';
     
+    Chat::Get().Update();
     m_pPlayers->Update();
+    m_pUI->Update();
     if(!g_bOfflinePLay) m_pGameNetwork->SendPlayerStatusFixedRate();
-    LogSystem::Get().Update();
-
 }
 
 void Game::DrawFrame()
@@ -105,7 +121,8 @@ void Game::DrawFrame()
         window.draw(m_ConnectingText);
     }
     window.draw(*m_pPlayers);
-    window.draw(LogSystem::Get());
+    window.draw(*m_pUI);
+    window.draw(Chat::Get());
     window.display();
 }
 
@@ -113,19 +130,47 @@ void Game::HandleEvents()
 {
     while (window.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
-            window.close();
-        if (event.type == sf::Event::KeyPressed)
+        switch (event.type)
         {
-            switch (event.key.code)
+        case sf::Event::Closed: window.close(); break;
+        case sf::Event::KeyPressed:
             {
-            case sf::Keyboard::I: m_pPlayers->EnableInterpolation(); break;
-            default: break;
-            }
-        }
-        if (event.type == sf::Event::Resized)
+                switch (event.key.code)
+                {
+                case sf::Keyboard::Up: m_PressedKeyCodes.push_front(1001); break;
+                case sf::Keyboard::Down: m_PressedKeyCodes.push_front(1002); break;
+                default: break;
+                }
+
+                if (Chat::Get().IsActive()) break;
+                switch (event.key.code)
+                {
+                case sf::Keyboard::I: m_pPlayers->EnableInterpolation(); break;
+                default: break;
+                }
+            }break;
+        case sf::Event::TextEntered:
         {
-            //window.setView(view);
+            m_PressedKeyCodes.push_front(event.text.unicode);
+        }break;
+        case sf::Event::Resized:
+            {
+                static sf::Vector2u prevWindowSize{ 0u, 0u };
+                if (prevWindowSize.x != window.getSize().x) {
+                    window.setSize(sf::Vector2u(window.getSize().x, window.getSize().x * (9.f / 16.f)));
+                    prevWindowSize = window.getSize();
+                }
+                else {
+                    window.setSize(sf::Vector2u(window.getSize().y * (16.f / 9.f), window.getSize().y));
+                    prevWindowSize = window.getSize();
+                }
+
+                //view.setSize(g_CamSizeY * ((float)window.getSize().x / (float)window.getSize().y), g_CamSizeY);
+                //view.setCenter((float)window.getSize().x / 2.f, (float)window.getSize().y / 2.f);
+                //window.setView(view);
+            }
+            break;
+        default:break;
         }
     }
 }
